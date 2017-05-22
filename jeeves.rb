fb60a9793eea5970d8175c83e97aa6f2deab61e8
Harman Singh <harman28@gmail.com>
2017-05-22 13:21:31 +0530

    Separates twilio code from app file

--- jeeves.rb
+++ jeeves.rb
@@ -1,20 +1,15 @@
 require 'json'
 require 'logging'
 require 'sinatra'
-require 'httparty'
-require 'twilio-ruby'
 
-logger = Logging.logger(STDOUT)
-logger.level = :info
+require_relative 'jeeves/twilio'
+
+@logger = Logging.logger(STDOUT)
+@logger.level = :info
 
 @env = settings.production? ? 'LIVE' : 'TEST'
 
-logger.info "Booting in #{@env} mode"
-
-Twilio.configure do |config|
-  config.account_sid = ENV["TWILIO_#{@env}_SID"]
-  config.auth_token  = ENV["TWILIO_#{@env}_TOKEN"]
-end
+@logger.info "Booting in #{@env} mode"
 
 get '/*' do
   "Jeeves is here, sir, and he's waiting for you.\n\n"\
@@ -22,44 +17,5 @@ get '/*' do
 end
 
 post '/twilio' do
-  query = get_query params
-  result = get_result query
-  response = build_response result
-  content_type 'text/xml'
-  response
-end
-
-def get_query params
-  logger.info "Params: #{params.to_s}"
-  params['Body']
-end
-
-def get_result query
-  logger.info "Query: #{query}"
-  response = HTTParty.get(
-    "https://www.googleapis.com/customsearch/v1",
-    query:
-    {
-      q: query,
-      cx: ENV['GOOGLE_CSE_ID'],
-      num: 2,
-      key: ENV['GOOGLE_CSE_KEY']
-    })
-  JSON.parse(response.body)
-end
-
-def build_response result
-  logger.debug "Result: #{result.to_s}"
-  string_result = get_string_result result
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message string_result
-  end
-  logger.info "Result: #{string_result}"
-  twiml.text
-end
-
-def get_string_result result
-  result['items'].inject([]) { |message,item|
-    message.push(item['title'],item['snippet'])
-  }.join("\n")
+  return twilio_hook params
 end
---
+++ jeeves/twilio.rb
@@ -0,0 +1,53 @@
+require 'sinatra'
+require 'httparty'
+require 'twilio-ruby'
+require 'logging'
+
+Twilio.configure do |config|
+  config.account_sid = ENV["TWILIO_#{@env}_SID"]
+  config.auth_token  = ENV["TWILIO_#{@env}_TOKEN"]
+end
+
+def twilio_hook params
+  return "lol"
+  query = get_query params
+  result = get_result query
+  response = build_response result
+  content_type 'text/xml'
+  response
+end
+
+def get_query params
+  @logger.info "Params: #{params.to_s}"
+  params['Body']
+end
+
+def get_result query
+  @logger.info "Query: #{query}"
+  response = HTTParty.get(
+    "https://www.googleapis.com/customsearch/v1",
+    query:
+    {
+      q: query,
+      cx: ENV['GOOGLE_CSE_ID'],
+      num: 2,
+      key: ENV['GOOGLE_CSE_KEY']
+    })
+  JSON.parse(response.body)
+end
+
+def build_response result
+  @logger.debug "Result: #{result.to_s}"
+  string_result = get_string_result result
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message string_result
+  end
+  @logger.info "Result: #{string_result}"
+  twiml.text
+end
+
+def get_string_result result
+  result['items'].inject([]) { |message,item|
+    message.push(item['title'],item['snippet'])
+  }.join("\n")
+end
